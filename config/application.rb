84ddc4857ea3d33ca4b57261882ccf2819875b29
Patrick Connor <patrick@itoursmart.com>
2011-12-06 14:45:59 -0600

    mobile.css

--- config/application.rb
+++ config/application.rb
@@ -44,5 +44,7 @@ module Glasshouseproject
 
     # Version of your assets, change this if you want to expire all your assets
     config.assets.version = '1.0'
+    
+    config.assets.precompile += ['mobile.css']
   end
 end
