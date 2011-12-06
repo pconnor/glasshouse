44a85e20268ee770af241cf75533d210ea833031
Patrick Connor <patrick@itoursmart.com>
2011-12-06 17:28:42 -0600

    more asset work

--- config/application.rb
+++ config/application.rb
@@ -45,6 +45,6 @@ module Glasshouseproject
     # Version of your assets, change this if you want to expire all your assets
     config.assets.version = '1.0'
     
-    config.assets.precompile += ['mobile.css']
+    config.assets.precompile += ['mobile.css' 'jqtouch.css']
   end
 end
